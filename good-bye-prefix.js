2f7ee6f398aecb3a263087dad5b65ab1145de08e
Weilao <qqq123026689@126.com>
2015-09-18 03:26:05 +0800

    good bye prefix for document

--- good-bye-prefix.js
+++ good-bye-prefix.js
@@ -4,30 +4,35 @@
 
     var _style = getComputedStyle(document.documentElement);
 
-    function isPropertySupported(prop) {
-        return typeof _style[prop] === 'string';
+    function isPropertySupported(root, prop) {
+        return prop in root;
     }
 
-    function getPrefixedProperty(prop) {
+    function getPrefixedProperty(root, prop) {
         var prefixedProp = '';
         ['webkit', 'Moz', 'ms', 'O'].forEach(function (prefix) {
             var Prop = prop[0].toUpperCase() + prop.substr(1);
-            if (isPropertySupported(prefix + Prop)) {
+            if (isPropertySupported(root, prefix + Prop)) {
                 prefixedProp = prefix + Prop;
             }
         });
         return prefixedProp;
     }
 
-    function goodByPrefix(prop) {
-        // Do nothing if already supported
-        if (isPropertySupported(prop)) return;
+    function goodByPrefix(root) {
+        return function (property) {
+            // Do nothing if already supported
+            if (isPropertySupported(root, property)) return;
 
-        var prefixedProperty = getPrefixedProperty(prop);
+            var prefixedProperty = getPrefixedProperty(root, property);
 
-        if (!prefixedProperty) return;
+            if (!prefixedProperty) return;
+            setAlia(root, property, prefixedProperty);
+        };
+    }
 
-        Object.defineProperty(CSSStyleDeclaration.prototype, prop, {
+    function setAlia(root, property, prefixedProperty) {
+        Object.defineProperty(root, property, {
             get: function () {
                 return this[prefixedProperty];
             },
@@ -37,8 +42,8 @@
         });
     }
 
-    function getSpecialProperty(prefix) {
-        return Array.prototype.slice.apply(_style)
+    function getSpecialCSSProperty(properties, prefix) {
+        return properties
             .filter(function (prop) {
                 return prop.indexOf('-' + prefix) === 0;
             })
@@ -51,8 +56,31 @@
             });
     }
 
-    getSpecialProperty('webkit').forEach(goodByPrefix);
-    getSpecialProperty('moz').forEach(goodByPrefix);
-    getSpecialProperty('ms').forEach(goodByPrefix);
-    getSpecialProperty('o').forEach(goodByPrefix);
+    function getSpecialDocumentProperty(properties, prefix) {
+        return properties
+            .filter(function (prop) {
+                return prop.indexOf(prefix) === 0;
+            })
+            .map(function (prop) {
+                return prop
+                    .replace(new RegExp('(' + prefix + ')(.)'), function (_, $1, $2) {
+                        return $2.toLowerCase();
+                    });
+            });
+    }
+
+    var styleProperties = Array.prototype.slice.apply(_style);
+    getSpecialCSSProperty(styleProperties, 'webkit').forEach(goodByPrefix(_style));
+    getSpecialCSSProperty(styleProperties, 'moz').forEach(goodByPrefix(_style));
+    getSpecialCSSProperty(styleProperties, 'ms').forEach(goodByPrefix(_style));
+    getSpecialCSSProperty(styleProperties, 'o').forEach(goodByPrefix(_style));
+
+    var documentProperties = [];
+    for (var key in document) {
+        documentProperties.push(key);
+    }
+    getSpecialDocumentProperty(documentProperties, 'webkit').forEach(goodByPrefix(document));
+    getSpecialDocumentProperty(documentProperties, 'moz').forEach(goodByPrefix(document));
+    getSpecialDocumentProperty(documentProperties, 'ms').forEach(goodByPrefix(document));
+    getSpecialDocumentProperty(documentProperties, 'o').forEach(goodByPrefix(document));
 })();
